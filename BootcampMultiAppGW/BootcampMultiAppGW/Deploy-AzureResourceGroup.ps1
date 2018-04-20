68fa40a7e5fa065de2f32d4441968ff25c3d67ef
Aben Samuel <aben44@hotmail.com>
2018-04-20 10:35:36 +0000

    tested working

--- BootcampMultiAppGW/BootcampMultiAppGW/Deploy-AzureResourceGroup.ps1
+++ BootcampMultiAppGW/BootcampMultiAppGW/Deploy-AzureResourceGroup.ps1
@@ -2,7 +2,7 @@
 
 Param(
     [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
-    [string] $ResourceGroupName = 'BootcampMultiAppGW',
+    [string] $ResourceGroupName = 'BootcampMultiAppGW2',
     [switch] $UploadArtifacts,
     [string] $StorageAccountName,
     [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',
